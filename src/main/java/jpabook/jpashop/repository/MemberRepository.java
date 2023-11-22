e15b095ae98aeb7df9797ecc30762b7717396255
LAPTOP-IIK6U1LR\asdfg <asdfg0992@gmail.com>
2023-11-23 01:14:57 +0900

    <feat> lesson13 회원 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import jpabook.jpashop.domain.Member;
+
+@Repository // 컴포넌트 스캔에 의해 자동으로 springbean으로 관리
+public class MemberRepository {
+
+	@PersistenceContext // JPA의 EntityManager를 Spring이 생성한 EntityManager에 주입
+	private EntityManager em;
+	
+	public void save(Member member) {
+		em.persist(member);
+		// 영속성 컨텍스트에 member 엔티티 넣음, 나중에 commit 되는 시점에 db insert 반영
+	}
+	
+	public Member findOne(Long id) {
+		// findOne : 단건조회, (Type, PK)
+		return em.find(Member.class, id);
+	}
+	
+	public List<Member> findAll(){
+		List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+		// JPQL 사용, from의 대상이 테이블이 아닌 엔티티
+		return result;
+	}
+	
+	public List<Member> findByName(String name){
+		return em.createQuery("select m from Member m where m.name = :name", Member.class)
+				.setParameter("name", name)
+				.getResultList();
+	}
+}
