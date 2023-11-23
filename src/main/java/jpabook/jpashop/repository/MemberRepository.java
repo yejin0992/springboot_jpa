ee1416c0c226f44ce6ecc9cc03724ef86f2c2817
LAPTOP-IIK6U1LR\asdfg <asdfg0992@gmail.com>
2023-11-23 23:30:13 +0900

    <feat> lesson14 회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -8,12 +8,16 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 
 @Repository // 컴포넌트 스캔에 의해 자동으로 springbean으로 관리
+@RequiredArgsConstructor
 public class MemberRepository {
 
-	@PersistenceContext // JPA의 EntityManager를 Spring이 생성한 EntityManager에 주입
-	private EntityManager em;
+	// @PersistenceContext // JPA의 EntityManager를 Spring이 생성한 EntityManager에 주입
+	// 원래 엔티티 매니저는 @persistenceContext 어노테이션을 사용해야 하는데 스프링부트 jpa가 autowired 지원하기 때문에
+	// 생성자로 넣을 멤버필드에 final 넣어주고 @RequiredArgsConstructor 어노테이션 사용할 수 있음
+	private final EntityManager em;
 	
 	public void save(Member member) {
 		em.persist(member);
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true) // 읽기 전용, 더티체킹 허용 x(데이터 변경 안함)
+@RequiredArgsConstructor // final 가지고 있는 필드만 가지고 생성자를 만들어줌, 자동 autowired
+public class MemberService {
+
+	private final MemberRepository memberRepository; // 변경할 일이 없기 때문에 final 넣으면 좋음
+	
+//	생성자 인젝션, 테스트 하기에도 편함, 자동 autowired됨, 이 역할을 해주는 어노테이션은 @RequiredArgsConstructor
+//	public MemberService(MemberRepository memberRepository) {
+//		this.memberRepository = memberRepository;
+//	}
+
+	// 회원가입
+	@Transactional // 트랜잭션 안에서 데이터변경이 있어야 함 //spring과 java가 제공하는 트랜잭션이 있는데 spring을 쓰는것을 권장
+	public Long join(Member member) {
+		validateDuplicateMember(member); // 중복 회원 검증
+		memberRepository.save(member);
+		return member.getId();
+	}
+	
+	private void validateDuplicateMember(Member member) {
+		List<Member> findMembers = memberRepository.findByName(member.getName());
+		if(!findMembers.isEmpty()) {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		}
+	}
+	
+	// 회원 전체 조회
+	public List<Member> findMembers(){
+		return memberRepository.findAll();
+	}
+	
+	public Member findOne(Long memberId) {
+		return memberRepository.findOne(memberId);
+	}
+	
+}
